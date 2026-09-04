92dab455f651a81c85061527123390fc0c10a707
Piriyaram2004 <piriyaram2004@gmail.com>
2026-09-04 17:19:08 +0530

    NOW Folder Repo finish

---
+++ PersonalDigitalVault.API/Repositories/Implementations/FolderRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalDigitalVault.API.Data;
+using PersonalDigitalVault.API.Models;
+using PersonalDigitalVault.API.Repositories.Interfaces;
+
+namespace PersonalDigitalVault.API.Repositories.Implementations
+{
+    public class FolderRepository : IFolderRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FolderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // Add New Folder
+        public async Task AddAsync(Folder folder)
+        {
+            await _context.Folders.AddAsync(folder);
+            
+        }
+        //Checking Folder Exist Using Id , ParentFolderId , NormalizedFolderName Findout Duplicates
+        public  async Task<bool> ExistsByNameAsync(int userId, int? parentFolderId, string normalizedFolderName)
+        {
+            return await _context.Folders.AnyAsync(f =>
+                f.UserId == userId &&
+                f.ParentFolderId == parentFolderId &&
+                f.NormalizedFolderName == normalizedFolderName &&
+                !f.IsDeleted);
+        }
+        // Get or Find the Folder ...
+        public  async Task<Folder?> GetByIdAsync(int folderId)
+        {
+            return await _context.Folders.FirstOrDefaultAsync(f => f.FolderId == folderId);
+        }
+    }
+}
---
+++ PersonalDigitalVault.API/Repositories/Interfaces/IFolderRepository.cs
@@ -0,0 +1,16 @@
+﻿using PersonalDigitalVault.API.Models;
+
+namespace PersonalDigitalVault.API.Repositories.Interfaces
+{
+    public interface IFolderRepository
+    {
+        Task<Folder?> GetByIdAsync(int folderId);
+
+        Task<bool> ExistsByNameAsync(
+            int userId,
+            int? parentFolderId,
+            string normalizedFolderName);
+
+        Task AddAsync(Folder folder);
+    }
+}
