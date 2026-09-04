0e7516e0ae1e4bf1357e9ee025a585f50c6fefbe
Sejohn <umapriyatharsini75@gmail.com.com>
2026-09-04 15:23:45 +0530

    feat(public-sharing): scaffold create share link flow

---
+++ PersonalDigitalVault.API/Repositories/Implementations/ShareLinkRepository.cs
@@ -0,0 +1,22 @@
+﻿using PersonalDigitalVault.API.Data;
+using PersonalDigitalVault.API.Models;
+using PersonalDigitalVault.API.Repositories.Interfaces;
+
+namespace PersonalDigitalVault.API.Repositories.Implementations
+{
+    public class ShareLinkRepository : IShareLinkRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShareLinkRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(ShareLink shareLink)
+        {
+            await _context.ShareLinks.AddAsync(shareLink);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
